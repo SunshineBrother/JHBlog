f731fe20a8c7b69c3044659056ac10b2cc4fcf32
yunna <yunna@xiaohuigedeMacBook-Pro.local>
2019-01-07 14:58:41 +0800

    国际化

---
+++ 设计模式/设计模式/建造者模式/创建者模式/创建者模式/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  创建者模式
+//
+//  Created by yunna on 2018/12/26.
+//  Copyright © 2018年 yunna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
